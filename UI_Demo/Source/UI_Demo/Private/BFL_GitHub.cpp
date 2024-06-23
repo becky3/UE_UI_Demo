bf5b65860da45df2df006637482d78e6cfd4e95a
becky <satoru-kibe@rhythmcan.com>
2024-06-23 18:54:15 +0900

    - GitHubにアクセスして取得した Json 文字列を Blueprintで返却 ※ リポジトリ名とかを文字一覧にしてリストに表示したい

--- UI_Demo/Source/UI_Demo/Private/BFL_GitHub.cpp
+++ UI_Demo/Source/UI_Demo/Private/BFL_GitHub.cpp
@@ -3,70 +3,7 @@
 
 #include "BFL_GitHub.h"
 
-
-
-void UBFL_GitHub::SearchRepositories(const FString& Keyword)
+UGitHubManager* UBFL_GitHub::CreateGitHubManager()
 {
-    FString Url = FString::Printf(TEXT("https://api.github.com/search/repositories?q=%s"), *Keyword);
-    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-    Request->SetURL(Url);
-    Request->SetVerb("GET");
-    Request->SetHeader("Content-Type", "application/json");
-    Request->SetHeader("User-Agent", "UnrealEngine");
-
-    Request->OnProcessRequestComplete().BindUObject(this, &UBFL_GitHub::OnSearchCompleted);
-
-    Request->ProcessRequest();
-}
-
-void UBFL_GitHub::OnSearchCompleted(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
-{
-    if (bWasSuccessful && Response->GetResponseCode() == 200)
-    {
-        FString JsonResponse = Response->GetContentAsString();
-        UE_LOG(LogTemp, Log, TEXT("Response: %s"), *JsonResponse);
-        //OnGitHubSearchCompleted.Broadcast(JsonResponse);
-    }
-    else
-    {
-        UE_LOG(LogTemp, Error, TEXT("Failed to get response or response is invalid."));
-        //OnGitHubSearchCompleted.Broadcast(TEXT(""));
-    }
-}
-
-
-void UBFL_GitHub::TestSearchRepositories(const FString& Keyword)
-{
-    FString Url = FString::Printf(TEXT("https://api.github.com/search/repositories?q=%s"), *Keyword);
-    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> Request = FHttpModule::Get().CreateRequest();
-    Request->SetURL(Url);
-    Request->SetVerb("GET");
-    Request->SetHeader("Content-Type", "application/json");
-    Request->SetHeader("User-Agent", "UnrealEngine");
-
-    Request->OnProcessRequestComplete().BindLambda([](FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
-        {
-            if (bWasSuccessful && Response->GetResponseCode() == 200)
-            {
-                TSharedPtr<FJsonObject> JsonObject;
-                TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
-                if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid())
-                {
-                    const TArray<TSharedPtr<FJsonValue>>& Items = JsonObject->GetArrayField("items");
-                    for (const TSharedPtr<FJsonValue>& Item : Items)
-                    {
-                        TSharedPtr<FJsonObject> RepositoryObject = Item->AsObject();
-                        FString Name = RepositoryObject->GetStringField("name");
-                        FString HtmlUrl = RepositoryObject->GetStringField("html_url");
-                        UE_LOG(LogTemp, Log, TEXT("Repository Name: %s, URL: %s"), *Name, *HtmlUrl);
-                    }
-                }
-            }
-            else
-            {
-                UE_LOG(LogTemp, Error, TEXT("Failed to get response or response is invalid."));
-            }
-        });
-
-    Request->ProcessRequest();
+    return NewObject<UGitHubManager>();
 }
--- UI_Demo/Source/UI_Demo/Public/BFL_GitHub.h
+++ UI_Demo/Source/UI_Demo/Public/BFL_GitHub.h
@@ -4,15 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-#include "HttpModule.h"
-#include "Interfaces/IHttpRequest.h"
-#include "Interfaces/IHttpResponse.h"
-#include "Json.h"
-#include "JsonUtilities.h"
-#include "JsonObjectConverter.h"
+#include "GitHubManager.h"
 #include "BFL_GitHub.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGitHubApiCompleted, const FString&, JsonResponse);
 
 /**
  * 
@@ -23,16 +17,9 @@ class UI_DEMO_API UBFL_GitHub : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
-    UFUNCTION(BlueprintCallable, Category = "GitHub")
-    void SearchRepositories(const FString& Keyword);
+    // APIリクエスト管理クラスのインスタンスを作成し、APIを呼び出す関数
+    UFUNCTION(BlueprintCallable, Category = "API")
+    static UGitHubManager* CreateGitHubManager();
 
-    //UPROPERTY(BlueprintAssignable, Category = "GitHub")
-    //FOnGitHubApiCompleted OnGitHubSearchCompleted;
-
-    UFUNCTION(BlueprintCallable, Category = "Debug")
-    void TestSearchRepositories(const FString& Keyword);
-
-private:
-    void OnSearchCompleted(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
 };
 
